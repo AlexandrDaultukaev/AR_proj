ad9cc868517afbad736db9a6671641747c15e0dd
Alex <60806892+AlexandrDaultukaev@users.noreply.github.com>
2021-05-03 16:36:56 +0700

    Add a 10-ruble with soundtrack

---
+++ AR_Proj/Assets/Music/Dumpmusic.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dumpmusic : MonoBehaviour
+{
+    public AudioSource d;
+    // Start is called before the first frame update
+    void Start()
+    {
+        d = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
